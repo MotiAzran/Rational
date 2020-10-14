8ebe1faccdaa70a691444c7be3c66d71471d6df9
Moti Azran <motiazran9@gmail.com>
2020-10-14 23:22:25 +0300

    Add documentation

--- src/com/moti/Main.java
+++ src/com/moti/Main.java
@@ -1,18 +1,30 @@
+/*
+ * Moti Azran
+ */
 package com.moti;
 
 import java.util.Scanner;
 
+/**
+ * The main class program
+ */
 public class Main {
 
+    /**
+     * The program entry point,
+     * Get from the user two rational numbers
+     * and prints the sum, difference, product, ...
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         Scanner stdin = new Scanner(System.in);
 
         // Get first fraction
         System.out.print("Enter numerator: ");
         int numerator = stdin.nextInt();
-        System.out.print("Enter denominator: ");
         int denominator = 0;
         do {
+            System.out.print("Enter denominator: ");
             denominator = stdin.nextInt();
             if (0 == denominator) {
                 System.out.println("Error: denominator can't be zero!");
@@ -24,8 +36,8 @@ public class Main {
         // Get second fraction
         System.out.print("Enter numerator: ");
         numerator = stdin.nextInt();
-        System.out.print("Enter denominator: ");
         do {
+            System.out.print("Enter denominator: ");
             denominator = stdin.nextInt();
             if (0 == denominator) {
                 System.out.println("Error: denominator can't be zero!");
@@ -34,6 +46,7 @@ public class Main {
 
         Rational frac2 = new Rational(numerator, denominator);
 
+        // Print fractions relations
         System.out.printf("%s > %s: %s\n", frac1, frac2, frac1.greaterThan(frac2)? "True" : "False");
         System.out.printf("%s == %s: %s\n", frac1, frac2, frac1.equals(frac2)? "True" : "False");
         System.out.printf("%s + %s = %s\n", frac1, frac2, frac1.plus(frac2).reduce());
--- src/com/moti/Rational.java
+++ src/com/moti/Rational.java
@@ -1,9 +1,26 @@
+/*
+ * Moti Azran
+ *
+ * Implements rational number class
+ */
 package com.moti;
 
+/**
+ * Rational number class.
+ * The numerator is an integer
+ * and the denominator is a positive integer
+ */
 public class Rational {
-    private int _numerator;
-    private int _denominator;
+    private final int _numerator;
+    private final int _denominator;
 
+    /**
+     * Initialize the rational number,
+     * if the denominator is equal to 0
+     * or less than 0, the number initializes to 0
+     * @param numerator the rational numerator
+     * @param denominator the rational denominator
+     */
     public Rational(int numerator, int denominator) {
         if (denominator <= 0) {
             // Invalid denominator set the fraction to zero
@@ -15,65 +32,115 @@ public class Rational {
         }
     }
 
+    /**
+     * Checks if this number is greater than
+     * the parameter number
+     * @param frac another rational for comparison
+     * @return true if this number is greater than frac number, otherwise false
+     */
     public boolean greaterThan(Rational frac) {
         return (_numerator * frac._denominator) > (frac._numerator * _denominator);
     }
 
+    /**
+     * Checks if this number is equal to the
+     * frac number
+     * @param frac another rational for comparison
+     * @return true if the numbers are equals, otherwise false
+     */
     public boolean equals(Rational frac) {
         return (_numerator * frac._denominator) == (frac._numerator * _denominator);
     }
 
+    /**
+     * Add this number to the frac number
+     * @param frac number to add
+     * @return The sum of both rationals
+     */
     public Rational plus(Rational frac) {
         // Calculate the numerator and denominator of the sum
-        int sum_numerator = (_numerator * frac._denominator) + (frac._numerator * _denominator);
-        int sum_denominator = _denominator * frac._denominator;
+        int sumNumerator = (_numerator * frac._denominator) + (frac._numerator * _denominator);
+        int sumDenominator = _denominator * frac._denominator;
 
-        return new Rational(sum_numerator, sum_denominator);
+        return new Rational(sumNumerator, sumDenominator);
     }
 
+    /**
+     * reduce frac number from this number
+     * @param frac number to reduce
+     * @return The difference of both rationals
+     */
     public Rational minus(Rational frac) {
         // Calculate the numerator and denominator of the difference
-        int diff_numerator = (_numerator * frac._denominator) - (frac._numerator * _denominator);
-        int diff_denominator = _denominator * frac._denominator;
+        int diffNumerator = (_numerator * frac._denominator) - (frac._numerator * _denominator);
+        int diffDenominator = _denominator * frac._denominator;
 
-        return new Rational(diff_numerator, diff_denominator);
+        return new Rational(diffNumerator, diffDenominator);
     }
 
+    /**
+     * Multiply this number to the frac number
+     * @param frac number to multiply
+     * @return The product of both rationals
+     */
     public Rational multiply(Rational frac) {
         // Calculate the numerator and denominator of the product
-        int prod_numerator = _numerator * frac._numerator;
-        int prod_denominator = _denominator * frac._denominator;
+        int prodNumerator = _numerator * frac._numerator;
+        int prodDenominator = _denominator * frac._denominator;
 
-        return new Rational(prod_numerator, prod_denominator);
+        return new Rational(prodNumerator, prodDenominator);
     }
 
+    /**
+     * Get the rational numerator
+     * @return the rational numerator
+     */
     public int getNumerator() {
         return _numerator;
     }
 
+    /**
+     * Get the rational denominator
+     * @return the rational denominator
+     */
     public int getDenominator() {
         return _denominator;
     }
 
+    /**
+     * Get string representation of the rational
+     * @return string representation of the rational
+     */
     public String toString() {
         return String.format("%d/%d", _numerator, _denominator);
     }
 
-    static private int gcd(int x, int y) {
-        if (0 == y) {
-            return x;
+    /**
+     * Get the greatest common divisor
+     * of num1 and num2
+     * @param num1 the first number
+     * @param num2 the second number
+     * @return the greatest common divisor of num1 and num2
+     */
+    static private int gcd(int num1, int num2) {
+        if (0 == num2) {
+            return num1;
         }
 
-        return gcd(y, x % y);
+        return gcd(num2, num1 % num2);
     }
 
+    /**
+     * Reduce the fraction
+     * @return the reduced fraction
+     */
     public Rational reduce() {
         // Find the greatest common divisor of the numerator and the denominator
-        int common_divisor = Math.abs(gcd(_numerator, _denominator));
+        int commonDivisor = Math.abs(gcd(_numerator, _denominator));
 
-        int reduced_numerator = _numerator / common_divisor;
-        int reduced_denominator = _denominator / common_divisor;
+        int reducedNumerator = _numerator / commonDivisor;
+        int reducedDenominator = _denominator / commonDivisor;
 
-        return new Rational(reduced_numerator, reduced_denominator);
+        return new Rational(reducedNumerator, reducedDenominator);
     }
 }
